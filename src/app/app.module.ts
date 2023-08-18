bb20b591bc1cf498a5c468adc48cd37e25e605b7
snadi043 <snadi043@uottawa.ca>
2023-08-18 09:39:17 -0400

    changes to the components, directory structure and created initial model files for th application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,31 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from 'src/components/ShoppingList/shoppingList.component';
-import { ShoppingListEditComponent } from 'src/components/ShoppingListEdit/shoppingListEdit.component';
-import { RecipeListComponent } from 'src/components/RecipeList/recipeList.component';
-import { RecipeItemComponent } from 'src/components/RecipeItem/recipeItem.component';
-import { RecipeDetailComponent } from 'src/components/RecipeDetail/recipeDetail.component';
-
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent,
+    HeaderComponent,
+    RecipesComponent,
     RecipeListComponent,
+    RecipeDetailComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
